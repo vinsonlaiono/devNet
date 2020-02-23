6dee556ca987b662aed4ff7364fa85bb838cd1e2
vinson aiono <vinson.aiono@gmail.com>
2020-02-22 19:41:19 -0800

    added post route to handle creating a new post

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,9 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth'); // protects our routes
+const { check, validationResult } = require('express-validator/check');
+
+const Post = require('../../models/Post');
+const User = require('../../models/Users');
+const Profile = require('../../models/Profile');
 
 // @route   Get api/posts
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => res.send('Posts route'));
+router.get(
+    '/',
+    [
+        auth,
+        check('text')
+            .not()
+            .isEmpty()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const user = User.findById(req.user.id).select('-password');
+
+            const newPost = new Post({
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            });
+
+            const post = await newPost.save();
+
+            res.json(post);
+        } catch (err) {
+            console.error(err);
+            res.status(500).send('Server error ...');
+        }
+    }
+);
 
 module.exports = router;
